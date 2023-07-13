eed2353f364788d8ec73f83cb69f318b4ae96ed7
soundsonacid <soundsonacid@gmail.com>
2023-07-13 17:08:05 -0500

    initialize marginfi account

--- programs/yield-program/src/instructions/passive.rs
+++ programs/yield-program/src/instructions/passive.rs
@@ -1,18 +1,39 @@
 use anchor_lang::prelude::*;
+// use marginfi::state::marginfi_account::MarginfiAccount;
+// use marginfi::state::marginfi_group::MarginfiGroup;
+use marginfi::program::Marginfi;
+// use kivo::state::user::User;
 
 use crate::state::lending_account::PassiveLendingAccount;
 
 pub const LENDING_ACCOUNT: &[u8] = b"passive_lending_account";
+pub const USER: &[u8] = b"user";
+pub const KIVO_MFI_ACCOUNT: &[u8] = b"kivo_mfi_account";
 
 #[derive(Accounts)]
 pub struct InitializePassiveLendingAccount<'info> {
-    /// CHECK: verified by CPI
+    /// CHECK: verified by pda derivation
+    #[account(
+        seeds = [
+            USER,
+            payer.key().as_ref(),
+        ],
+        bump,
+    )]
     pub kivo_account: UncheckedAccount<'info>,
 
-    /// CHECK: awaiting marginfi-cpi
+    /// CHECK: verified by mfi cpi call
+    #[account(
+        mut,
+        seeds = [
+            KIVO_MFI_ACCOUNT,
+            kivo_account.key().as_ref(),
+        ],
+        bump,
+    )]
     pub marginfi_account: UncheckedAccount<'info>,
 
-    /// CHECK: awaiting marginfi-cpi
+    /// CHECK: verified by mfi cpi call
     pub marginfi_group: UncheckedAccount<'info>,
 
     #[account(
@@ -25,23 +46,21 @@ pub struct InitializePassiveLendingAccount<'info> {
         ],
         bump,
     )]
-    pub lending_account: Account<'info, PassiveLendingAccount>,
+    pub passive_lending_account: Account<'info, PassiveLendingAccount>,
 
     #[account(mut)]
     pub payer: Signer<'info>,
 
-    // pub kivo_program: Program<'info, Kivo>,
-
-    // pub marginfi_program: Program<'info, MarginFi>,
+    pub marginfi_program: Program<'info, Marginfi>,
 
     pub system_program: Program<'info, System>,
 }
 
 // #[derive(Accounts)]
 // pub struct PassiveLendingDeposit<'info> {
-        // pub kivo_account: UncheckedAccount<'info>,
+    // pub kivo_account: UncheckedAccount<'info>,
 
-        // pub marginfi_account: UncheckedAccount<'info>,
+    // pub marginfi_account: UncheckedAccount<'info>,
 
     // pub kivo_program: Program<'info, Kivo>,
 
--- programs/yield-program/src/lib.rs
+++ programs/yield-program/src/lib.rs
@@ -11,42 +11,33 @@ pub mod instructions;
 pub mod kivo_yield_program {
     use super::*;
 
-    pub fn handle_intialize_passive_lending_account(ctx: Context<InitializePassiveLendingAccount>, bump: u8) -> Result<()> {
-        // let kivo_account = &mut ctx.accounts.kivo_account;
-        // let marginfi_account = &mut ctx.accounts.marginfi_account;
-        // let marginfi_group = &ctx.accounts.marginfi_group;
-        // let lending_account = &mut ctx.accounts.passive_lending_account;
-        // let system_program = &ctx.accounts.system_program;
-        // let marginfi_program: &ctx.accounts.marginfi_program;
+    pub fn handle_initialize_passive_lending_account(ctx: Context<InitializePassiveLendingAccount>, bump: u8) -> Result<()> {
+        let signature_seeds = kivo::state::user::User::get_user_signer_seeds(&ctx.accounts.payer.key, &bump);
+        let signer_seeds = &[&signature_seeds[..]];  
 
+        let init_margin_acc = marginfi::cpi::accounts::MarginfiAccountInitialize {
+            marginfi_group: ctx.accounts.marginfi_group.to_account_info(),
+            marginfi_account: ctx.accounts.marginfi_account.to_account_info(),
+            authority: ctx.accounts.kivo_account.to_account_info(),
+            fee_payer: ctx.accounts.payer.to_account_info(),
+            system_program: ctx.accounts.system_program.to_account_info(),
+        };
 
-        // let signature_seeds = User::get_user_signer_seeds(&ctx.accounts.payer.key, &bump);
-        // let signer_seeds = &[&signature_seeds[..]];  
+        let init_margin_acc_ctx = CpiContext::new_with_signer(
+            ctx.accounts.marginfi_program.to_account_info().clone(),
+            init_margin_acc,
+            signer_seeds
+        );
 
-        // let init_margin_acc = MarginfiAccountInitialize {
-        //     marginfi_group: marginfi_group,
-        //     marginfi_account: marginfi_account,
-        //     authority: kivo_account,
-        //     fee_payer: kivo_account,
-        //     system_program: system_program,
-        // }
+        marginfi::cpi::marginfi_account_initialize(init_margin_acc_ctx)?;
 
-        // let init_margin_acc_ctx = CpiContext::new_with_signer(
-        //     marginfi_program.to_account_info().clone(),
-        //     init_margin_acc,
-        //     marginfi_program
-        // );
-
-        // marginfi_account_initialize(init_margin_acc_ctx, signer_seeds)?;
-
-        // lending_account.new(
-        //     kivo_account.key(),
-        //     marginfi_account.key(),
-        //     marginfi_group.key(),
-        // )?;
+        ctx.accounts.passive_lending_account.new(
+            ctx.accounts.kivo_account.key(),
+            ctx.accounts.marginfi_account.key(),
+            ctx.accounts.marginfi_group.key(),
+        )?;
 
         Ok(())
     }
-
 }   
 
